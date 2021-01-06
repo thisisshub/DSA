5a20e38099252cf667cdc76fdd5475a2094842c2
thisisshub <thisisshub@gmail.com>
2021-01-06 12:13:58 +0530

    Add palindrome and gcd test in tests.py

--- tests.py
+++ tests.py
@@ -11,10 +11,25 @@ from A_mathematics import (
 )
  
 class Test_A_mathematics:
-    def test_count_digits(self):
+
+    def test_A_count_digits(self):
         assert A_count_digits.method1(-1234) == 4
         assert A_count_digits.method1(0) == 1
         assert A_count_digits.method1(1234) == 4
         assert A_count_digits.method2(-1234) == 4
         assert A_count_digits.method2(0) == 1
         assert A_count_digits.method2(1234) == 4
+
+
+    def test_B_palindrome(self):
+        assert B_palindrome.method1('aibohphobia') == True
+        assert B_palindrome.method1('string') == False
+
+
+    def test_C_gcd(self):
+        assert C_gcd.method1(-123, -32) == 1
+        assert C_gcd.method1(12, 18) == 6
+        assert C_gcd.method1(-4, 14) == 2
+        assert C_gcd.method2(-123, -32) == 1
+        assert C_gcd.method2(12, 18) == 6
+        assert C_gcd.method2(-4, 14) == 2
