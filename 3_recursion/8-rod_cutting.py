f4f5d974f28a6a2c8c03ab7510b87299260b2929
thisisshub <thisisshub@gmail.com>
2021-01-06 08:22:50 +0530

    Create rod_cutting_problem.py

---
+++ 3_recursion/8-rod_cutting.py
@@ -0,0 +1,22 @@
+def method1(price: list, n: int) -> int:
+    """
+    >>> prices = [1, 5, 8, 9, 10, 17, 17, 20]
+    >>> method1(prices, len(prices))
+    22
+    """
+    import sys
+    if n <= 0: return 0
+    max_val = -sys.maxsize - 1
+    for i in range(0, n):
+        max_val = max(max_val, price[i] + 
+                        method1(price, n - i -1))
+    return max_val
+
+if __name__ == '__main__':
+    """
+    from timeit import timeit
+    prices = [1, 5, 8, 9, 10, 17, 17, 20]
+    print(timeit(lambda: method1(prices, len(prices)), number=10000)) # 1.6864704039999197
+    """
+    import doctest
+    doctest.testmod()
