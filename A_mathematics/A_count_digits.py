aac1e6170712ebcf5a55e9ff7bb46e7a30c7ba17
thisisshub <thisisshub@gmail.com>
2021-01-06 11:33:04 +0530

    Remove doctest comments

--- A_mathematics/A_count_digits.py
+++ A_mathematics/A_count_digits.py
@@ -1,24 +1,8 @@
 def method1(n: int) -> int:
-    """
-    >>> method1(-1234567890)
-    10
-    >>> method1(1234567890)
-    10
-    >>> method1(0)
-    1
-    """
     return len(str(n)) if n>=0 else len(str(n))-1
 
 
 def method2(n: int) -> int:
-    """
-    >>> method2(1234567890)
-    10
-    >>> method2(-1234567890)
-    10
-    >>> method2(0)
-    1
-    """
     import math
     return (int(math.log10(n)+1 if n>0 else(1 if n==0 else math.log10(-n)+1)))
 
@@ -28,5 +12,3 @@ if __name__ == '__main__':
     print(timeit(lambda: method1(1234567890), number=10000)) # 0.0022839140001451597
     print(timeit(lambda: method2(1234567890), number=10000)) # 0.004222848001518287
     """
-    import doctest
-    doctest.testmod()
--- A_mathematics/C_gcd.py
+++ A_mathematics/C_gcd.py
@@ -1,32 +1,10 @@
 def method1(n: int, m: int) -> int:
-    """
-    >>> method1(-123, -321)
-    3
-    >>> method1(123, 321)
-    3
-    >>> method1(-123, -321)
-    3
-    >>> method1(-123, 321)
-    3
-    >>> method1(123, -321)
-    3
-    """
     while m:
         n, m = m, n%m
     return abs(n)
 
 
 def method2(n: int, m: int) -> int:
-    """
-    >>> method2(123, 321)
-    3
-    >>> method2(-123, -321)
-    3
-    >>> method2(-123, 321)
-    3
-    >>> method2(123, -321)
-    3
-    """
     from math import gcd
     return gcd(n, m)
 
@@ -37,5 +15,3 @@ if __name__ == '__main__':
     print(timeit(lambda: method2(3, 9), number=10000))
     print(timeit(lambda: method1(3, 9), number=10000))
     """
-    import doctest
-    doctest.testmod()
