34edb2e11b184332aa7a681c751cb28eb2345cb7
thisisshub <thisisshub@gmail.com>
2021-01-06 11:34:15 +0530

    Add time in gcd.py with timeit

--- A_mathematics/C_gcd.py
+++ A_mathematics/C_gcd.py
@@ -12,6 +12,6 @@ def method2(n: int, m: int) -> int:
 if __name__ == '__main__':
     """
     from timeit import timeit
-    print(timeit(lambda: method2(3, 9), number=10000))
-    print(timeit(lambda: method1(3, 9), number=10000))
+    print(timeit(lambda: method1(3, 9), number=10000)) # 0.0018165660003433004
+    print(timeit(lambda: method2(3, 9), number=10000)) # 0.006539128997246735
     """
