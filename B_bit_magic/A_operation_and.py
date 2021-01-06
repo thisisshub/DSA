3086bca53ba3f7660819a1567d3c846f2fc68277
thisisshub <thisisshub@gmail.com>
2021-01-06 15:40:02 +0530

    Remove doctest and comments

--- B_bit_magic/A_operation_and.py
+++ B_bit_magic/A_operation_and.py
@@ -1,12 +1,4 @@
 def method1(n: int, m: int) -> bin:
-    """
-    >>> method1(25, 30)
-    '0b11000'
-    >>> method1(76, 30)
-    '0b1100'
-    >>> method1(60, 13)
-    '0b1100
-    """
     return bin(n & m)
 
 
@@ -15,5 +7,3 @@ if __name__ == '__main__':
     from timeit import timeit
     print(timeit(lambda: method1(25, 30), number=10000)) # 0.0016055950000009034
     """
-    import doctest
-    doctest.testmod()
