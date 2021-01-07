f73552c999ac9b4ef374dda93ec3a0d3eb067f47
thisisshub <thisisshub@gmail.com>
2021-01-07 10:56:39 +0530

    Modified

---
+++ D_arrays/types_of_arrays/B_dynamic_size_array.py
@@ -0,0 +1,9 @@
+def method1(n: list):
+    return n
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1([1, 2, 3, 4, 5]), number=10000)) # 0.0021682340002371348
+    """
