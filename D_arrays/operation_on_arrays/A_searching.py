a93cbd036a83f47a68fcec5a4148326b74c51f76
thisisshub <thisisshub@gmail.com>
2021-01-07 11:22:04 +0530

    Create searching .py

---
+++ D_arrays/operation_on_arrays/A_searching.py
@@ -0,0 +1,31 @@
+def method1(l: list, low: int, high: int, x: int) -> int:
+    if high >= low:
+        mid = (high + low) // 2
+        if l[mid] == x:
+            return mid
+        elif l[mid] > x:
+            return method1(l, low, mid - 1, x)
+        else:
+            return method1(l, mid + 1, high, x)
+    else:
+        return -1
+    return method1(l, 0, len(l) - 1, x)
+
+
+def method2(n: int, l: list) -> bool:
+    return True if l.index(n) <= len(l) else False
+
+
+def method3(n: int, l: list) -> bool:
+    return True if n in l else False
+
+
+if __name__ == "__main__":
+    """
+    n = 9999
+    l = [i for i in range(10000)] # [0, 1, 3, ...., 9999]
+    from timeit import timeit
+    print(timeit(lambda: method1(l, 0, len(l)-1, n), number=10000)) # 0.03099031199963065
+    print(timeit(lambda: method2(n, l), number=10000)) # 0.9109301280004729
+    print(timeit(lambda: method3(n, l), number=10000)) # 0.8249549619995378
+    """
--- tests/test_D_arrays.py
+++ tests/test_D_arrays.py
@@ -3,6 +3,8 @@ import sys
 sys.path.append("../")
 
 from D_arrays.types_of_arrays import A_fixed_size_array, B_dynamic_size_array
+from D_arrays.operation_on_arrays import A_searching
+
 
 class Test_D_arrays:
     def test_A_fixed_array(self):
@@ -13,3 +15,10 @@ class Test_D_arrays:
         assert len(B_dynamic_size_array.method1(n)) == 0
         n = [1, 2, 3, 4, 5, 6]
         assert len(B_dynamic_size_array.method1(n)) == 6
+
+    def test_A_searching(self):
+        n = 9
+        l = [i for i in range(10)]
+        assert A_searching.method1(l, 0, len(l) - 1, n) == 9
+        assert A_searching.method2(n, l) == True
+        assert A_searching.method3(n, l) == True
