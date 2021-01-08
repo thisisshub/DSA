8080c2a576c487c71c5bf2712c0b664da315d072
thisisshub <thisisshub@gmail.com>
2021-01-08 09:53:34 +0530

    Create multidimensionalarray.py

---
+++ G_matrix/A_multidimensionalarray.py
@@ -0,0 +1,25 @@
+def method1(l: list) -> list:
+    l = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 0]]
+    return l
+
+
+def method2(l: list) -> list:
+    l = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 0]]
+    for i in l:
+        return i
+
+
+def method3(l: list) -> list:
+    l = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 0]]
+    for i in l:
+        for j in i:
+            print(j, end=' ')
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(l), number=10000)) # 0.005068514998129103
+    print(timeit(lambda: method2(l), number=10000)) # 0.0051237920015410054
+    print(timeit(lambda: method3(l), number=10000)) # 0.1331835500022862
+    """
