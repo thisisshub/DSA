f2ee21a836a9233635915fbd8edcb409a21ada1a
thisisshub <thisisshub@gmail.com>
2021-01-08 12:27:51 +0530

    Create  G_matrix/D_transposeamatrix.py

---
+++ G_matrix/D_transposeamatrix.py
@@ -0,0 +1,19 @@
+def method1():
+    matrix = [[1, 2, 3], 
+              [4, 5, 6], 
+              [8, 9, 0]]
+    
+    ans = [[0, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0]]
+        
+    for i in range(len(matrix)):
+        for j in range(3):
+            ans[j][i] = matrix[i][j]
+
+    for r in ans:
+        print(r)
+
+if __name__ == "__main__":
+    from timeit import timeit
+    print(timeit(lambda: method1(), number=10000)) # 0.4282153890017071
