4ccca667e624b75b5258ff6aaa79780dadc9344a
thisisshub <thisisshub@gmail.com>
2021-01-09 20:27:17 +0530

    Create E_rotatingamatrix.py

---
+++ G_matrix/E_rotateamatrix.py
@@ -0,0 +1,34 @@
+def method1():
+    N = 4
+    
+    # mat = [[0 for x in range(N)] for y in range(N)] 
+    mat = [ [1, 2, 3, 4 ], 
+        [5, 6, 7, 8 ], 
+        [9, 10, 11, 12 ], 
+        [13, 14, 15, 16 ] ]
+    
+    def rotateMatrix(mat): 
+        for x in range(0, int(N / 2)): 
+            for y in range(x, N-x-1): 
+                temp = mat[x][y] 
+                mat[x][y] = mat[y][N-1-x] 
+                mat[y][N-1-x] = mat[N-1-x][N-1-y] 
+                mat[N-1-x][N-1-y] = mat[N-1-y][x] 
+                mat[N-1-y][x] = temp 
+
+
+
+    def displayMatrix(mat): 
+        for i in range(0, N): 
+            for j in range(0, N): 
+                print (mat[i][j], end = ' ') 
+            print ("")
+        return rotateMatrix(mat)
+    
+    return (rotateMatrix(mat), displayMatrix(mat))
+    
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(), number=10000)) # 0.2696803380001711
+    """
