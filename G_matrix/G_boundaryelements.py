24acd72b84971f11777d10fe17d6054252a95ec6
thisisshub <thisisshub@gmail.com>
2021-01-09 20:45:33 +0530

    Create  G_matrix/H_spiralprint.py and format to code style black

--- G_matrix/G_boundaryelements.py
+++ G_matrix/G_boundaryelements.py
@@ -12,10 +12,11 @@ def method1():
                     print(a[i][j]),
                 else:
                     print(" "),
-            print( )
-    a = [[1, 2, 3, 4], [5, 6, 7, 8], 
-         [1, 2, 3, 4], [5, 6, 7, 8]]
+            print()
+
+    a = [[1, 2, 3, 4], [5, 6, 7, 8], [1, 2, 3, 4], [5, 6, 7, 8]]
     return printBoundary(a, 4, 4)
+
 
 if __name__ == "__main__":
     """
