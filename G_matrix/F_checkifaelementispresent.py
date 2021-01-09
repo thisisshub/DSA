d49ebb815324b664402b2cc84e03517a35fbc46b
thisisshub <thisisshub@gmail.com>
2021-01-09 20:36:15 +0530

    Create G_matrix/F_checkifaelementispresent.py

---
+++ G_matrix/F_checkifaelementispresent.py
@@ -0,0 +1,26 @@
+def method1():
+    def search(mat, n, x):
+        i = 0
+        j = n - 1
+        while i < n and j >= 0:
+            if mat[i][j] == x:
+                print("n Found at ", i, ", ", j)
+                return 1
+            if mat[i][j] > x:
+                j -= 1
+            else:
+                i += 1
+
+        print("Element not found")
+        return 0
+
+    mat = [[10, 20, 30, 40], [15, 25, 35, 45], [27, 29, 37, 48], [32, 33, 39, 50]]
+
+    return search(mat, 4, 29)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(), number=10000)) # 0.06679722199987737
+    """
