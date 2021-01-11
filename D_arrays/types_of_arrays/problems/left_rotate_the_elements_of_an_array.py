5b8c3183fea98b0e65eacc2794dc4f10f4aa038c
thisisshub <thisisshub@gmail.com>
2021-01-11 18:27:04 +0530

    Create D_arrays/types_of_arrays/problems/left_rotate_the_elements_of_an_array.py

---
+++ D_arrays/types_of_arrays/problems/left_rotate_the_elements_of_an_array.py
@@ -0,0 +1,15 @@
+def method1():
+    a = [1, 2, 3, 4, 5]
+    for i in range(1):
+        f = a[0]
+        for j in range(0, len(a) - 1):
+            a[j] = a[j + 1]
+        a[len(a) - 1] = f
+    return a
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(), number=10000)) 0.008410404003370786
+    """
