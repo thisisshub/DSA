54eefe7202974e4fe4841d83ff709c6ffde7ed1a
thisisshub <thisisshub@gmail.com>
2021-01-13 09:15:09 +0530

    Create D_arrays/problems/max_difference_in_array.py

---
+++ D_arrays/problems/max_difference_in_array.py
@@ -0,0 +1,20 @@
+def method1():
+    def maxDiff(ll: list) -> int:
+        vmin = ll[0]
+        dmax = 0
+        for i in range(len(ll)):
+            if ll[i] < vmin:
+                vmin = ll[i]
+            elif ll[i] - vmin > dmax:
+                dmax = ll[i] - vmin
+        return dmax
+
+    ll = [i for i in range(5)]
+    return maxDiff(ll)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(), number=10000)) # 0.01388289500027895
+    """
