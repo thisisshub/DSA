22f96b22401f36458e6459fa3704d8f501eb39c0
thisisshub <thisisshub@gmail.com>
2021-01-15 09:26:47 +0530

    Create D_arrays/problems/maximum_consecutive_ones.py

---
+++ D_arrays/problems/maximum_consecutive_ones.py
@@ -0,0 +1,21 @@
+def method1(ll: list, n: int) -> int:
+    count = 0
+    result = 0
+    for i in range(0, n):
+        if ll[i] == 0:
+            count = 0
+        else:
+            count += 1
+            result = max(result, count)
+
+    return result
+
+
+ll = [1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1]
+n = len(ll)
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(ll, n), number=10000)) # 0.02980456100340234
+    """
