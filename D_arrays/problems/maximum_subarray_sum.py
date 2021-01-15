ff13d35a0923ffe4a90ebf0c4d0f29e42db44b0f
thisisshub <thisisshub@gmail.com>
2021-01-15 09:31:18 +0530

    Create D_arrays/problems/maximum_subarray_sum.py

---
+++ D_arrays/problems/maximum_subarray_sum.py
@@ -0,0 +1,22 @@
+def method1(ll, n):
+
+    max_so_far = 0
+    max_ending_here = 0
+
+    for i in range(0, n):
+        max_ending_here = max_ending_here + ll[i]
+        if max_ending_here < 0:
+            max_ending_here = 0
+        elif max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+
+    return max_so_far
+
+
+if __name__ == "__main__":
+    """
+    ll = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, -7]
+    n = len(ll)
+    from timeit import timeit
+    print(timeit(lambda: method1(ll, n), number=10000)) # 0.024287710002681706
+    """
