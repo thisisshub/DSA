ff17abc8491b57abc5b8e4fd2a26572362e254ff
thisisshub <thisisshub@gmail.com>
2021-01-15 10:01:05 +0530

    Create D_arrays/problems/majority_element.py

--- D_arrays/problems/majority_element.py
+++ D_arrays/problems/majority_element.py
@@ -1,27 +1,26 @@
 def method1():
     def findCandidate(A):
-	maj_index = 0
-	count = 1
-	for i in range(len(A)):
-		if A[maj_index] == A[i]:
-			count += 1
-		else:
-			count -= 1
-		if count == 0:
-			maj_index = i
-			count = 1
-	return A[maj_index]
+        maj_index = 0
+        count = 1
+        for i in range(len(A)):
+            if A[maj_index] == A[i]:
+                count += 1
+            else:
+                count -= 1
+            if count == 0:
+                maj_index = i
+                count = 1
+        return A[maj_index]
 
     def isMajority(A, cand):
         count = 0
         for i in range(len(A)):
             if A[i] == cand:
                 count += 1
-        if count > len(A)/2:
+        if count > len(A) / 2:
             return True
         else:
             return False
-
 
     def printMajority(A):
         cand = findCandidate(A)
@@ -30,6 +29,12 @@ def method1():
         else:
             print("No Majority Element")
 
-
     A = [1, 3, 3, 1, 2]
     return printMajority(A)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(), number=10000)) # 0.12490123600218794
+    """
