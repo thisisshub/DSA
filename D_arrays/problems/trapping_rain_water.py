ba683002305a4b5e8d55935473a8c145e92901d5
thisisshub <thisisshub@gmail.com>
2021-01-15 09:21:26 +0530

    Create D_arrays/problems/trapping_rain_water.py

---
+++ D_arrays/problems/trapping_rain_water.py
@@ -0,0 +1,26 @@
+def method1():
+    def findWater(arr, n):
+        left = [0] * n
+        right = [0] * n
+        water = 0
+        left[0] = arr[0]
+        for i in range(1, n):
+            left[i] = max(left[i - 1], arr[i])
+        right[n - 1] = arr[n - 1]
+        for i in range(n - 2, -1, -1):
+            right[i] = max(right[i + 1], arr[i])
+        for i in range(0, n):
+            water += min(left[i], right[i]) - arr[i]
+        return water
+
+    arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    n = len(arr)
+
+    return findWater(arr, n)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1(), number=10000)) # 0.13557261999812908
+    """
