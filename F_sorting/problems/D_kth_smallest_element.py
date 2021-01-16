765fe3f28efa3112455ec1039cd05d974e2c2128
thisisshub <thisisshub@gmail.com>
2021-01-16 14:36:03 +0530

    Create D_kth_smallest_element.py

---
+++ F_sorting/problems/D_kth_smallest_element.py
@@ -0,0 +1,28 @@
+def method1(arr, n, k):
+    arr.sort()
+    return arr[k - 1]
+
+
+def method2(arr, k):
+    import heapq
+
+    smallest = []
+    for value in arr:
+        if len(smallest) < k:
+            heapq.heappush(smallest, -value)
+        else:
+            heapq.heappushpop(smallest, -value)
+    if len(smallest) < k:
+        return None
+    return -smallest[0]
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    arr = [12, 3, 5, 7, 19]
+    n = len(arr)
+    k = 2
+    print(timeit(lambda: method1(arr, n, k), number=10000))  # 0.0018417680003040005
+    print(timeit(lambda: method2(arr, k), number=10000))  # 0.009863699999186792
+    """
