28e82e8d0b202681b5df9c826776550092333d5b
thisisshub <thisisshub@gmail.com>
2021-01-16 07:01:20 +0530

    Create E_searching/problems/count_of_1s_in_a_binary_sorted_array.py

---
+++ E_searching/problems/count_of_1s_in_a_binary_sorted_array.py
@@ -0,0 +1,20 @@
+def method1(arr: list, low: int, high: int) -> int:
+
+    if high >= low:
+        mid = low + (high - low) // 2
+        if (mid == high or arr[mid + 1] == 0) and (arr[mid] == 1):
+            return mid + 1
+        if arr[mid] == 1:
+            return method1(arr, (mid + 1), high)
+
+        return method1(arr, low, mid - 1)
+
+    return 0
+
+
+if __name__ == "__main__":
+    """
+    arr=[1, 1, 1, 1, 0, 0, 0]
+    from timeit import timeit
+    print(timeit(lambda: method1(arr, 0, len(arr)-1), number=10000)) # 0.00268922500026747
+    """
