1413d3ed6e25c2d905f69144ec44695db444d93a
thisisshub <thisisshub@gmail.com>
2021-01-18 18:26:14 +0530

    Create H_hashing/M_more_than_nk_occurences.py

---
+++ H_hashing/M_more_than_nk_occurences.py
@@ -0,0 +1,21 @@
+def method1(ll: list, n: int, k: int) -> int:
+    x = n // k
+    freq = {}
+    for i in range(n):
+        if ll[i] in freq:
+            freq[ll[i]] += 1
+        else:
+            freq[ll[i]] = 1
+    for i in freq:
+        if freq[i] > x:
+            print(i)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    ll = [1, 1, 2, 2, 3, 5, 4, 2, 2, 3, 1, 1, 1]
+    n = len(ll)
+    k = 4
+    print(timeit(lambda: method1(ll, n, k), number=10000))  # 0.1085869129965431
+    """
