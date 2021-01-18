2e4fa139d5ff842511e4e370586a9371f4156326
thisisshub <thisisshub@gmail.com>
2021-01-18 16:47:22 +0530

    Create H_hashing/D_union_of_two_unsorted_arrays.py

---
+++ H_hashing/D_union_of_two_unsorted_arrays.py
@@ -0,0 +1,21 @@
+def method1(arr1, arr2, n1, n2):
+    hs = set()
+    for i in range(0, n1):
+        hs.add(arr1[i])
+    for i in range(0, n2):
+        hs.add(arr2[i])
+    print("Union:")
+    for i in hs:
+        print(i, end=" ")
+    print("\n")
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    arr1 = [7, 1, 5, 2, 3, 6]
+    arr2 = [3, 8, 6, 20, 7]
+    n1 = len(arr1)
+    n2 = len(arr2)
+    print(timeit(lambda: method1(arr1, arr2, n1, n2), number=10000)) # 0.22361706300580408
+    """
