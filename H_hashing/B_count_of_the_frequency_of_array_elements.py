72a63cd27ddde03be93280fb46b26a82073a88ed
thisisshub <thisisshub@gmail.com>
2021-01-18 16:35:14 +0530

    Create B_count_of_the_frequency_of_array_elements.py

---
+++ H_hashing/B_count_of_the_frequency_of_array_elements.py
@@ -0,0 +1,21 @@
+def method1(ll: list, n: int) -> int:
+
+    mp = dict()
+
+    for i in range(n):
+        if ll[i] in mp.keys():
+            mp[ll[i]] += 1
+        else:
+            mp[ll[i]] = 1
+
+    for x in mp:
+        print(x, " ", mp[x])
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    ll = [10, 20, 20, 10, 10, 20, 5, 20]
+    n = len(ll)
+    print(timeit(lambda: method1(ll, n), number=10000)) # 0.24235440699703759
+    """
