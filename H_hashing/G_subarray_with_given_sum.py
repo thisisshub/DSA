eac935ad2702b3a730a506fa188bb0cef46f4467
thisisshub <thisisshub@gmail.com>
2021-01-18 18:26:22 +0530

    Format to style black code

--- H_hashing/G_subarray_with_given_sum.py
+++ H_hashing/G_subarray_with_given_sum.py
@@ -1,26 +1,27 @@
-def method1(ll: list, n: int, sum: int) -> int: 
-	curr_sum = ll[0] 
-	start = 0
-	i = 1
-	while i <= n: 
-		while curr_sum > sum and start < i-1: 		
-			curr_sum = curr_sum - ll[start] 
-			start += 1
-		if curr_sum == sum: 
-			print ("Sum found between indexes") 
-			print ("% d and % d"%(start, i-1)) 
-			return 1
-		if i < n: 
-			curr_sum = curr_sum + ll[i] 
-		i += 1
-	print ("No subarray found") 
-	return 0
+def method1(ll: list, n: int, sum: int) -> int:
+    curr_sum = ll[0]
+    start = 0
+    i = 1
+    while i <= n:
+        while curr_sum > sum and start < i - 1:
+            curr_sum = curr_sum - ll[start]
+            start += 1
+        if curr_sum == sum:
+            print("Sum found between indexes")
+            print("% d and % d" % (start, i - 1))
+            return 1
+        if i < n:
+            curr_sum = curr_sum + ll[i]
+        i += 1
+    print("No subarray found")
+    return 0
+
 
 if __name__ == "__main__":
     """
     from timeit import timeit
-    ll = [15, 2, 4, 8, 9, 5, 10, 23] 
-    n = len(ll) 
+    ll = [15, 2, 4, 8, 9, 5, 10, 23]
+    n = len(ll)
     sum = 23
     print(timeit(lambda: method1(ll, n, sum), number=10000)) # 0.28271653299452737
     """
