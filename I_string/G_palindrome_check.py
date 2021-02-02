31ed9d88abb716e6450102b0251db048207c20aa
thisisshub <thisisshub@gmail.com>
2021-02-02 15:13:01 +0530

    Create G_palindrome_check

---
+++ I_string/G_palindrome_check.py
@@ -0,0 +1,9 @@
+def method1(s: str) -> bool:
+    return True if s == s[::-1] else False
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1("AABAA"), number=10000)) # 0.0018115240000042832
+    """
