2740e9249e6a96799fd02205ba1576d34b840336
thisisshub <thisisshub@gmail.com>
2021-02-02 15:08:26 +0530

    Create F_given_two_strings_check_if_they_are_rotations_of_each_other_or_not

---
+++ I_string/F_given_two_strings_check_if_they_are_rotations_of_each_other_or_not.py
@@ -0,0 +1,12 @@
+def method1(str1: str, str2: str) -> bool:
+    if len(str1) != len(str2):
+        return False
+    temp = str1 + str2
+    return True if temp.count(str2) else False
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1("ABCD", "DCBA"), number=10000)) # 0.0039531319998786785
+    """
