5408f1352c7e93ff07251d349d97169296c4f4a1
thisisshub <thisisshub@gmail.com>
2021-02-02 13:48:25 +0530

    Create A_given_a_string_check_if_they_are_an_anagram_of_each_other

---
+++ I_string/A_given_a_string_check_if_they_are_an_anagram_of_each_other.py
@@ -0,0 +1,35 @@
+def method1(s1: str, s2: str) -> bool:
+    if len(s1) != len(s2):
+        return False
+
+    str1 = sorted(s1)
+    str2 = sorted(s2)
+
+    for i in range(len(s1)):
+        if str1[i] != str2[i]:
+            return False
+    return True
+
+
+def method2(s1: str, s2: str) -> bool:
+    count1 = [0] * 256
+    count2 = [0] * 256
+    for i in s1:
+        count1[ord(i)] += 1
+    for i in s2:
+        count2[ord(i)] += 1
+
+    if len(s1) != len(s2):
+        return False
+    for i in range(256):
+        if count1[i] != count2[i]:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    print(timeit(lambda: method1("thisisastring", "stringthisisa"), number=10000)) # 0.015203513999949791
+    print(timeit(lambda: method2("thisisastring", "stringthisisa"), number=10000)) # 0.12443715199970029
+    """
