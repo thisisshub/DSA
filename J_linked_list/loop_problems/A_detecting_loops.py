033c8aa9abc4059e083c30fb429667d2fa9dbf4b
thisisshub <thisisshub@gmail.com>
2021-02-04 02:48:47 +0530

    Create A_detecting_loops.py

---
+++ J_linked_list/loop_problems/A_detecting_loops.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+
+    def detectLoop(self):
+        s = set()
+        temp = self.head
+        while temp:
+            if temp in s:
+                return True
+            s.add(temp)
+            temp = temp.next
+        return False
+
+
+llist = LinkedList()
+llist.push(1)
+llist.push(2)
+llist.push(3)
+llist.push(4)
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+    llist.head.next.next.next.next = llist.head
+    if llist.detectLoop():
+        print(timeit(lambda: llist.detectLoop(), number=10000)) # 0.005041514000140523
+    else:
+        print('No loop')
+    """
