947969bb801a07ef2e44007dfa1fe018810cea51
thisisshub <thisisshub@gmail.com>
2021-02-03 15:10:46 +0530

    Format style to black

--- J_linked_list/A_implementation.py
+++ J_linked_list/A_implementation.py
@@ -1,11 +1,10 @@
-class Node: 
-    
+class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
+
 class LinkedList:
-    
     def __init__(self):
         self.head = None
 
