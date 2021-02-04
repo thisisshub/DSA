f7837e8aa289702cb64d8e441a997545341dbabf
thisisshub <thisisshub@gmail.com>
2021-02-04 08:06:15 +0530

    Create J_lru_cache_design.py

---
+++ J_linked_list/problems/J_lru_cache_design.py
@@ -0,0 +1,36 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+
+if __name__ == "__main__":
+    from timeit import timeit
+
+    cache = LRUCache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    cache.get(1)
+    cache.put(3, 3)
+    cache.get(2)
+    cache.put(4, 4)
+    cache.get(1)
+    cache.get(3)
+    cache.get(4)
+    print(timeit(lambda: cache.cache, number=10000))  # 0.0005562089991144603
