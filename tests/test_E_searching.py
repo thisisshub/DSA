3cc64ec465c3cfa8d842c23f27d5379421da3abe
thisisshub <thisisshub@gmail.com>
2021-02-04 11:09:40 +0530

    Fix test in tests/test_E_searching.py

--- tests/test_E_searching.py
+++ tests/test_E_searching.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("../")
 
 from E_searching.iterative import A_binary_search_iterative
-from E_searching.recursive import B_binary_search_recursive
+from E_searching.recursive import A_binary_search_recursive
 
 class Test_E_searching:
     def test_A_binary_search_iterative(self):
@@ -14,4 +14,4 @@ class Test_E_searching:
     def test_B_binary_search_recursive(self):
         n = 9
         l = [i for i in range(10)]
-        assert B_binary_search_recursive.method1_recursive(l, 0, len(l) - 1, n) == 9
+        assert A_binary_search_recursive.method1_recursive(l, 0, len(l) - 1, n) == 9
