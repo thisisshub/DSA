7eb2736f3788092d981364c707d4bf99c78f3217
thisisshub <thisisshub@gmail.com>
2021-02-05 04:29:32 +0530

    Create L_queue/A_implementation.py

---
+++ L_queue/A_implementation.py
@@ -0,0 +1,24 @@
+def method1(queue: list) -> list:
+
+    queue.append("a")
+    queue.append("b")
+    queue.append("c")
+
+    print("Initial queue")
+    print(queue)
+
+    print("\nElements dequeued from queue")
+    print(queue.pop(0))
+    print(queue.pop(0))
+    print(queue.pop(0))
+
+    print("\nQueue after removing elements")
+    return queue
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    print(timeit(lambda: method1([]), number=10000)) # 0.8325513340023463
+    """
