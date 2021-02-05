1e7f4ea57b3922034c7626f3a85e9f942229a967
thisisshub <thisisshub@gmail.com>
2021-02-05 12:55:09 +0530

    Create C_postorder_traversal.py

---
+++ N_tree/implementation/C_postorder_traversal.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+def method1(root):
+
+    if root:
+
+        method1(root.left)
+
+        method1(root.right)
+
+        print(root.val),
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+
+    print(timeit(lambda: method1(root), number=10000))  # 0.23595180499978596
+    """
