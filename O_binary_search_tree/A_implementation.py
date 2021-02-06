e4e842f4782c7dd140acd1bc488bd260f38d1ebf
thisisshub <thisisshub@gmail.com>
2021-02-06 12:10:36 +0530

    Create A_implementation.py

---
+++ O_binary_search_tree/A_implementation.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    r = Node(50)
+    r = insert(r, 30)
+    r = insert(r, 20)
+    r = insert(r, 40)
+    r = insert(r, 70)
+    r = insert(r, 60)
+    r = insert(r, 80)
+
+    print(timeit(lambda: inorder(r), number=10000))  # 0.4426064440012851
+    """
+
