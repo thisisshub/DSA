bb2a37196ec5248dd1ecf867a8ca8bb6593d76f5
thisisshub <thisisshub@gmail.com>
2021-02-06 13:24:01 +0530

    Create D_check_for_bst.py

---
+++ O_binary_search_tree/problems/D_check_for_bst.py
@@ -0,0 +1,40 @@
+INT_MAX = 4294967296
+INT_MIN = -4294967296
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def isBST(node):
+    return isBSTUtil(node, INT_MIN, INT_MAX)
+
+
+def isBSTUtil(node, mini, maxi):
+
+    if node is None:
+        return True
+
+    if node.data < mini or node.data > maxi:
+        return False
+
+    return isBSTUtil(node.left, mini, node.data - 1) and isBSTUtil(
+        node.right, node.data + 1, maxi
+    )
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    root = Node(4)
+    root.left = Node(2)
+    root.right = Node(5)
+    root.left.left = Node(1)
+    root.left.right = Node(3)
+
+    print(timeit(lambda: isBST(root), number=10000))  # 0.01445196800341364
+    """
