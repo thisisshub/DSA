eb7a39d06256b67e0ed1178d65b01f2536ee0aa7
thisisshub <thisisshub@gmail.com>
2021-02-07 19:58:55 +0530

    Create B_dfs_based_algorithm.py

---
+++ Q_graphs/problems/topological_sorting/B_dfs_based_algorithm.py
@@ -0,0 +1,19 @@
+graph = {"A": ["B", "C"], "B": ["D", "E"], "C": ["F"], "D": [], "E": ["F"], "F": []}
+
+visited = set()
+
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    print(timeit(lambda: dfs(visited, graph, "A"), number=10000)) # 0.001466015000914922
+    """
