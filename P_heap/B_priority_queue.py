d4200bb96a07f2b90bf536909f92b6ee6aa88fd3
thisisshub <thisisshub@gmail.com>
2021-02-07 19:23:03 +0530

    Create B_priority_queue.py

---
+++ P_heap/B_priority_queue.py
@@ -0,0 +1,38 @@
+class PriorityQueue(object):
+    def __init__(self):
+        self.queue = []
+
+    def __str__(self):
+        return " ".join([str(i) for i in self.queue])
+
+    def isEmpty(self):
+        return len(self.queue) == 0
+
+    def insert(self, data):
+        self.queue.append(data)
+
+    def delete(self):
+        try:
+            max = 0
+            for i in range(len(self.queue)):
+                if self.queue[i] > self.queue[max]:
+                    max = i
+            item = self.queue[max]
+            del self.queue[max]
+            return item
+        except IndexError:
+            print()
+            exit()
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    myQueue = PriorityQueue()
+    myQueue.insert(12)
+    myQueue.insert(1)
+    myQueue.insert(14)
+    myQueue.insert(7)
+    print(timeit(lambda: myQueue, number=10000))  # 0.0003760149993468076
+    """
