9ec6b937ff27b8194df86484890487bff5096187
thisisshub <thisisshub@gmail.com>
2021-02-07 19:19:00 +0530

    Create C_k_largest_elements.py

---
+++ P_heap/problems/C_k_largest_elements.py
@@ -0,0 +1,25 @@
+import heapq
+
+
+class Solution(object):
+    def find_Kth_Largest(self, nums, k):
+
+        h = []
+        for e in nums:
+            heapq.heappush(h, (-e, e))
+        for i in range(k):
+            w, e = heapq.heappop(h)
+            w = w
+            if i == k - 1:
+                return e
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    arr_nums = [12, 14, 9, 50, 61, 41]
+    s = Solution()
+    result = s.find_Kth_Largest(arr_nums, 3)
+    print(timeit(lambda: result, number=10000))  # 0.0005090839986223727
+    """
