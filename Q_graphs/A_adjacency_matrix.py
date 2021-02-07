ead527de1f3459776518250ad4fa57299324356c
thisisshub <thisisshub@gmail.com>
2021-02-07 20:09:10 +0530

    Create A_adjacency_matrix.py

---
+++ Q_graphs/A_adjacency_matrix.py
@@ -0,0 +1,47 @@
+class Graph(object):
+    def __init__(self, size):
+        self.adjMatrix = []
+        for _ in range(size):
+            self.adjMatrix.append([0 for i in range(size)])
+        self.size = size
+
+    def add_edge(self, v1, v2):
+        if v1 == v2:
+            print("Same vertex %d and %d" % (v1, v2))
+        self.adjMatrix[v1][v2] = 1
+        self.adjMatrix[v2][v1] = 1
+
+    def remove_edge(self, v1, v2):
+        if self.adjMatrix[v1][v2] == 0:
+            print("No edge between %d and %d" % (v1, v2))
+            return
+        self.adjMatrix[v1][v2] = 0
+        self.adjMatrix[v2][v1] = 0
+
+    def __len__(self):
+        return self.size
+
+    def print_matrix(self):
+        for row in self.adjMatrix:
+            for val in row:
+                print("{:4}".format(val)),
+            print
+
+
+def main():
+    g = Graph(5)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 2)
+    g.add_edge(2, 0)
+    g.add_edge(2, 3)
+
+    g.print_matrix()
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    print(timeit(lambda: main(), number=10000))  # 1.204392577001272
+    """
