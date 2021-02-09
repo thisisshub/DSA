d1947b18cd629e7920a77a1db48538bdf56a696b
thisisshub <thisisshub@gmail.com>
2021-02-09 10:48:28 +0530

    Create C_job_sequencing_problem.py

---
+++ R_greedy/C_job_sequencing_problem.py
@@ -0,0 +1,33 @@
+def method1(arr, t):
+
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(n - 1 - i):
+            if arr[j][2] < arr[j + 1][2]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    result = [False] * t
+
+    job = ["-1"] * t
+
+    for i in range(len(arr)):
+
+        for j in range(min(t - 1, arr[i][1] - 1), -1, -1):
+
+            if result[j] is False:
+                result[j] = True
+                job[j] = arr[i][0]
+                break
+
+    print(job)
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    arr = [["a", 2, 100], ["b", 1, 19], ["c", 2, 27], ["d", 1, 25], ["e", 3, 15]]
+
+    print(timeit(lambda: method1(arr, 3), number=10000))  # 0.08790500099712517
+    """
