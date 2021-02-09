a475054d6c631673ac8e417a7e2fcbdba7273dca
thisisshub <thisisshub@gmail.com>
2021-02-09 10:48:28 +0530

    Create A_activity_selection_problem.py

---
+++ R_greedy/A_activity_selection_problem.py
@@ -0,0 +1,22 @@
+def method1(s, f):
+    n = len(f)
+    print("The following activities are selected")
+
+    i = 0
+    print(i, end=" ")
+
+    for j in range(n):
+
+        if s[j] >= f[i]:
+            print(j, end=" ")
+            i = j
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    s = [1, 3, 0, 5, 8, 5]
+    f = [2, 4, 6, 7, 9, 9]
+    print(timeit(lambda: method1(s, f), number=10000))  # 0.2884501590015134
+    """
