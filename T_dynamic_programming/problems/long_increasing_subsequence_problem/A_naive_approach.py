f478abc7b6ef82a278476e573adb7e7edda2842a
thisisshub <thisisshub@gmail.com>
2021-02-09 13:02:37 +0530

    Create A_naive_approach.py

---
+++ T_dynamic_programming/problems/long_increasing_subsequence_problem/A_naive_approach.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        tails = [0 for i in range(len(nums))]
+        size = 0
+        for x in nums:
+            i = 0
+            j = size
+            while i != j:
+                mid = i + (j - i) // 2
+                if tails[mid] < x:
+                    i = mid + 1
+                else:
+                    j = mid
+                    tails[i] = x
+                    size = max(i + 1, size)
+        return size
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    ob1 = Solution()
+    print(
+        timeit(lambda: ob1.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]), number=10000)
+    )  # 0.006747746003384236
+    """
