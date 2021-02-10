53ba9637df25ce73cc74f261a206562152a7ad8a
thisisshub <thisisshub@gmail.com>
2021-02-10 18:51:39 +0530

    Fix C_recursion/E_tail_recursion.py

--- C_recursion/E_tail_recursion.py
+++ C_recursion/E_tail_recursion.py
@@ -1,7 +1,3 @@
-
-last operation of a function is a recursive call. 
-
-
 def tailrecursive(n: int, a=1) -> int:
     if n == 0:
         return a
@@ -9,20 +5,15 @@ def tailrecursive(n: int, a=1) -> int:
 
 
 def nontailrecursive(n: int) -> int:
-    
-    
-    
-    
-    
-    
+
     if n == 0:
         return 1
     return n * nontailrecursive(n - 1)
 
 
 if __name__ == "__main__":
-    
+
     from timeit import timeit
-    print(timeit(lambda: tailrecursive(10), number=10000)) 
-    print(timeit(lambda: nontailrecursive(10), number=10000)) 
-    
+
+    print(timeit(lambda: tailrecursive(10), number=10000))
+    print(timeit(lambda: nontailrecursive(10), number=10000))
