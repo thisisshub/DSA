2189a416a4de6d3aae6278b507f3700e88959d4e
thisisshub <thisisshub@gmail.com>
2021-02-10 06:42:12 +0530

    Create A_representation.py

---
+++ U_trie/A_representation.py
@@ -0,0 +1,19 @@
+_end = "_end_"
+
+
+def method1(*words):
+    root = dict()
+    for word in words:
+        current_dict = root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict[_end] = _end
+    return root
+
+
+if __name__ == "__main__":
+    """
+    from timeit import timeit
+
+    print(timeit(lambda: method1("foo", "bar", "bob", "cat")))  # 1.2939369230007287
+    """
