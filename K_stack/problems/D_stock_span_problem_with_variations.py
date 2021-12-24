044eed6d31fef7e878adc78543a8a79312d8f77e
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2021-12-25 01:26:06 +0530

    Explanatory comments added

--- K_stack/problems/D_stock_span_problem_with_variations.py
+++ K_stack/problems/D_stock_span_problem_with_variations.py
@@ -1,4 +1,9 @@
 def method1(price, n, S):
+    """
+    An algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+    The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backward) for which the stock price was less than or equal to today's price.
+    For example, if the price of a stock over the next 7 days were [100,80,60,70,60,75,85], then the stock spans would be [1,1,1,2,1,4,6].
+    """
     S[0] = 1
     for i in range(1, n, 1):
         S[i] = 1
